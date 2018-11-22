a78b43a9d25510432984915bed193f68ed848d5d
Trung Le <trungle@Trungs-MacBook-Pro.local>
2018-11-22 09:42:49 +0700

    add init login status to fb

--- src/providers/facebook-login-provider.ts
+++ src/providers/facebook-login-provider.ts
@@ -18,24 +18,26 @@ export class FacebookLoginProvider extends BaseLoginProvider {
   initialize(): Promise<SocialUser> {
     return new Promise((resolve, reject) => {
       this.loadScript(this.loginProviderObj, () => {
-          FB.init({
-            appId: this.clientId,
-            autoLogAppEvents: true,
-            cookie: true,
-            xfbml: true,
-            version: 'v2.10'
-          });
-          FB.AppEvents.logPageView();
-
-          FB.getLoginStatus(function (response: any) {
-            if (response.status === 'connected') {
-              const accessToken = FB.getAuthResponse()['accessToken'];
-              FB.api('/me?fields=name,email,picture', (res: any) => {
-                resolve(FacebookLoginProvider.drawUser(Object.assign({}, {token: accessToken}, res)));
-              });
-            }
-          });
+        FB.init({
+          appId: this.clientId,
+          autoLogAppEvents: true,
+          cookie: true,
+          xfbml: true,
+          version: 'v2.10'
         });
+
+        FB.AppEvents.logPageView();
+
+        FB.getLoginStatus(function (response: any) {
+          if (response.status === 'connected') {
+            const accessToken = FB.getAuthResponse()['accessToken'];
+            FB.api('/me?fields=name,email,picture', (res: any) => {
+              resolve(FacebookLoginProvider.drawUser(Object.assign({}, { token: accessToken }, res)));
+            });
+          }
+        });
+
+      });
     });
   }
 
